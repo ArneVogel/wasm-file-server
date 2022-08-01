ea429ac3cc6a45f2461dbb2a4d5a821b44cbbd10
Arne Vogel <arnevogel96@gmail.com>
2022-08-01 22:22:18 +0200

    counter

--- file-server/src/main.rs
+++ file-server/src/main.rs
@@ -1,14 +1,11 @@
-#![deny(warnings)]
-
 use hyper::server::conn::Http;
 use tokio::net::TcpListener;
 use std::fs;
+use std::sync::{Arc};
+use std::sync::atomic::AtomicUsize;
 
 use hyper::service::service_fn;
 use hyper::{Body, Method, Request, Response, Result, StatusCode};
-
-static NOTFOUND: &[u8] = b"Not Found";
-
 
 #[cfg(not(target_os = "wasi"))]
 async fn get_tcplistener() -> TcpListener {
@@ -24,16 +21,26 @@ async fn get_tcplistener() -> TcpListener {
     TcpListener::from_std(stdlistener).unwrap()
 }
 
+#[derive(Debug)]
+struct State {
+    visitors: AtomicUsize,
+    likes: AtomicUsize,
+}
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
     let listener = get_tcplistener().await;
+    let state = Arc::new(State{visitors: AtomicUsize::new(0), likes: AtomicUsize::new(0)});
 
     loop {
         let (stream, _) = listener.accept().await?;
+        let state = state.clone();
 
         tokio::task::spawn(async move {
             if let Err(err) = Http::new()
-                .serve_connection(stream, service_fn(response_examples))
+                .serve_connection(stream, service_fn(move |req| {
+                    router(req, state.clone())
+                }))
                 .await
             {
                 println!("Failed to serve connection: {:?}", err);
@@ -42,13 +49,21 @@ async fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
     }
 }
 
-async fn response_examples(req: Request<Body>) -> Result<Response<Body>> {
+
+async fn router(req: Request<Body>, state: Arc<State>) -> Result<Response<Body>> {
     match (req.method(), req.uri().path()) {
+        (&Method::POST, "/like") => {
+            state.likes.fetch_add(1, std::sync::atomic::Ordering::AcqRel);
+            Ok(inc_like())
+        }
+
+        (&Method::GET, "/") | (&Method::GET, "/index.html") => {
+            state.visitors.fetch_add(1, std::sync::atomic::Ordering::AcqRel);
+            return_index(state.clone()).await
+        }
         (&Method::GET, anything) => {
+            state.visitors.fetch_add(1, std::sync::atomic::Ordering::AcqRel);
             let mut s = String::from(anything);
-            if s == "/" {
-                s.push_str("index.html");
-            }
             match anything.starts_with("/") {
                 true => s.insert_str(0, "public"),
                 _ => return Ok(not_found()),
@@ -59,11 +74,19 @@ async fn response_examples(req: Request<Body>) -> Result<Response<Body>> {
     }
 }
 
+/// count likes
+fn inc_like() -> Response<Body> {
+    Response::builder()
+        .status(301)
+        .header("Location", "/")
+        .body("Back home!".into()).unwrap_or(not_found())
+}
+
 /// HTTP status code 404
 fn not_found() -> Response<Body> {
     Response::builder()
         .status(StatusCode::NOT_FOUND)
-        .body(NOTFOUND.into())
+        .body("Not Found".into())
         .unwrap()
 }
 
@@ -73,5 +96,15 @@ async fn simple_file_send(filename: &str) -> Result<Response<Body>> {
         return Ok(Response::new(body));
     }
     println!("could not open \"{}\": {:?}", filename, fs::read_to_string(filename).err());
+    Ok(not_found())
+}
+
+async fn return_index(state: Arc<State>) -> Result<Response<Body>> {
+    if let Ok(contents) = fs::read_to_string("public/index.html") {
+        let contents = contents.replace("{{visitors}}", format!("{:?}", state.visitors).as_str());
+        let contents = contents.replace("{{likes}}", format!("{:?}", state.likes).as_str());
+        let body = contents.into();
+        return Ok(Response::new(body));
+    }
     Ok(not_found())
 }
